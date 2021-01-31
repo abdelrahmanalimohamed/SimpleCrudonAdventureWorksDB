5a8886bc05fe70abd92c521311680c4192646f0b
Abdelrahamn Ali <abdelrahmanali.mohamed@outlook.com>
2021-02-01 00:56:03 +0200

    updatee

--- simpleCrudonAdventureWorksDB/Repository/Repository.cs
+++ simpleCrudonAdventureWorksDB/Repository/Repository.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using simpleCrudonAdventureWorksDB.Interfaces;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace simpleCrudonAdventureWorksDB.Repository
