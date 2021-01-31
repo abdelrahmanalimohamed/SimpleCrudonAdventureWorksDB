469f156ed18fd4f02db717a0089596bb25495409
Abdelrahamn Ali <abdelrahmanali.mohamed@outlook.com>
2021-02-01 00:48:47 +0200

    Add project files.

---
+++ simpleCrudonAdventureWorksDB/Models/Departments.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace simpleCrudonAdventureWorksDB.Models
+{
+    public class Departments
+    {
+        public int DepartmentID { get; set; }
+
+        public string Name { get; set; }
+
+        public string GroupName { get; set; }
+    }
+}
---
+++ simpleCrudonAdventureWorksDB/Repository/Repository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using simpleCrudonAdventureWorksDB.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace simpleCrudonAdventureWorksDB.Repository
+{
+    public class Repository<TDbContext> : IRepository where TDbContext : DbContext
+    {
+
+        protected TDbContext DbContext;
+
+        public Repository(TDbContext context)
+        {
+            DbContext = context;
+        }
+        public async Task CreateAsync<T>(T entity) where T : class
+        {
+            this.DbContext.Set<T>().Add(entity);
+            _ = await this.DbContext.SaveChangesAsync();
+        }
+
+        public Task DeleteAsync<T>(T entity) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<T>> SelectAll<T>() where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> SelectById<T>(long id) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync<T>(T entity) where T : class
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
